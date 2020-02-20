cb05dcfa52c13e04fd55259e68558cebce5ae5fc
rsoriano <rsorianosanchis@gmail.com>
2020-02-20 13:07:53 +0100

    components grund arkitektur/grid gjort, grund bootstrap stil

--- src/components/tasks/ListTasks.js
+++ src/components/tasks/ListTasks.js
@@ -3,10 +3,10 @@ import Task from './Task';
 
 const ListTasks = () => {
   const projectTasks = [
-    { taskname: 'Tomates', state: true },
-    { taskname: 'Bread', state: true },
-    { taskname: 'Fisk', state: false },
-    { taskname: 'Potatis', state: true }
+    { taskName: 'Tomates', state: true },
+    { taskName: 'Bread', state: true },
+    { taskName: 'Fisk', state: false },
+    { taskName: 'Potatis', state: true }
   ];
   return (
     <div className='card mx-3'>
@@ -16,13 +16,18 @@ const ListTasks = () => {
         <li className='list-group-item'>Dapibus ac facilisis in</li>
         <li className='list-group-item'>Vestibulum at eros</li> */}
         {projectTasks.length === 0 ? (
-          <li className='list-group-item-info'>
+          <li className='list-group-item bg-info'>
             There is not tasks for this project
           </li>
         ) : (
-          projectTasks.map(task => <Task key={task.taskname} task={task} />)
+          projectTasks.map(task => <Task key={task.taskName} task={task} />)
         )}
       </ul>
+      <div className='card-footer mt-0 d-flex justify-content-center'>
+        <button type='button' className='btn btn-sm btn-danger'>
+          Delete Project
+        </button>
+      </div>
     </div>
   );
 };
--- src/components/tasks/Task.js
+++ src/components/tasks/Task.js
@@ -1,7 +1,27 @@
 import React from 'react';
 
-const Task = () => {
-  return <p>from task</p>;
+const Task = ({ task }) => {
+  return (
+    <li className='list-group-item d-flex justify-content-between align-items-center'>
+      <p className='m-0 flex-grow-1'>{task.taskName}</p>
+
+      {task.state ? (
+        <button type='button' className='btn btn-sm btn-success'>
+          Complete
+        </button>
+      ) : (
+        <button type='button' className='btn btn-sm btn-danger'>
+          Incomplete
+        </button>
+      )}
+      <button type='button' className='btn btn-sm btn-warning mx-1'>
+        Edit
+      </button>
+      <button type='button' className='btn btn-sm btn-danger'>
+        &times;
+      </button>
+    </li>
+  );
 };
 
 export default Task;
